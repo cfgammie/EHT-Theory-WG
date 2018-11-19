f7ce341ea8c0a44435271892faf273dacdad1850
jadexter <jadexter@gmail.com>
2018-11-19 23:56:11 +0100

    added LP comparison

--- jadexter/fit_image_data.py
+++ jadexter/fit_image_data.py
@@ -9,6 +9,7 @@ import ehtim as eh
 import matplotlib
 matplotlib.use('Agg')
 matplotlib.rcParams['image.cmap']='inferno'
+matplotlib.rcParams['lines.markersize']=5
 
 # sefdfac = 0 is recommended to use actual noise in data
 sefdfac=0.
@@ -19,13 +20,13 @@ def read_img(imgfile,imgformat):
     if imgformat=='ipole':
         data = np.genfromtxt(imgfile)
         img = data[:,3:].transpose().reshape((4,160,160))
-    if imgformat=='fits':
+#    if imgformat=='fits':
         
     return img
 
 # calculate chi^2 for a polarized model image and data file using eht-imaging to simulate data from the model with gain errors and leakage
 # also do rough cut on the idealized data
-def run(file,imgfile,imgformat,pixsize,imgname,nflux=4,fluxmin=0.2,fluxmax=0.8,npa=2,pamin=90.,pamax=75.,fullfit=False,N=10):
+def run(file,imgfile,imgformat,pixsize,imgname,nflux=4,fluxmin=0.2,fluxmax=0.8,npa=2,pamin=90.,pamax=75.,fullfit=False,N=10,):
     img = read_img(imgfile,imgformat)
     obsall = eh.obsdata.load_uvfits(file,remove_nan=True)
     obsall.add_scans()
@@ -33,6 +34,7 @@ def run(file,imgfile,imgformat,pixsize,imgname,nflux=4,fluxmin=0.2,fluxmax=0.8,n
     pa=pamin+(pamax-pamin)*np.arange(npa)/(npa-1.)
     first_null = []; bump_amp = []
     visamp_list = []; visamperr_list = []; cphase_list = []; sigmacp_list = []
+    lpamp_list = []; lpamperr_list = []
     chi2 = []; res = []; imglist = []
     for p in pa:
         print p
@@ -59,10 +61,11 @@ def run(file,imgfile,imgformat,pixsize,imgname,nflux=4,fluxmin=0.2,fluxmax=0.8,n
                 obs_sim_perfect = obs_sim_perfect[0]
                 obs_sim_perfect.add_cphase()
                 obs_list,im,obs_new = simulate_obs.run(obs,img,pixsize,sefdfac=sefdfac,N=N,scan_avg=True,pa=p,fluxscl=f)
-                visamp,cphase,visamperr,sigmacp,ruv,u,v,ruvmax = get_values(obs_list)
-                res.append(calc_likelihood(ruv,visamp,visamperr,cphase,sigmacp,obs,obs_sim_perfect))
+                visamp,cphase,lpamp,visamperr,sigmacp,lpamperr,ruv,u,v,ruvmax = get_values(obs_list)
+                res.append(calc_likelihood(ruv,visamp,visamperr,cphase,sigmacp,lpamp,lpamperr,obs,obs_sim_perfect))
                 imglist.append(img)
                 visamp_list.append(visamp); cphase_list.append(cphase)
+                lpamp_list.append(lpamp); lpamperr_list.append(lpamperr)
                 visamperr_list.append(visamperr); sigmacp_list.append(sigmacp)
                 chi2.append(res[-1]['chi2'])
     
@@ -71,7 +74,7 @@ def run(file,imgfile,imgformat,pixsize,imgname,nflux=4,fluxmin=0.2,fluxmax=0.8,n
 #        print fluxscl[indx], chi2, chi2[indx]
         print indx, len(chi2), len(imglist), len(visamp_list), len(visamperr_list), len(cphase_list), len(sigmacp_list)
 #            plt.plot(np.sqrt(obs.data['u']**2.+obs.data['v']**2.),np.abs(obs.data['vis']))
-        plot_model_data(obs,ruv,visamp_list[indx],visamperr_list[indx],ruvmax,cphase_list[indx],sigmacp_list[indx],imgname+'_'+str(int(p)))
+        plot_model_data(obs,ruv,visamp_list[indx],visamperr_list[indx],ruvmax,cphase_list[indx],sigmacp_list[indx],lpamp_list[indx],lpamperr_list[indx],imgname+'_'+str(int(p)),res[indx]['chi2_amp'],res[indx]['chi2_cphase'],res[indx]['chi2_lp_amp'])
         best_image = imglist[indx]
         print np.shape(best_image)
         image_plot(best_image[0],imgname)
@@ -90,7 +93,7 @@ def image_plot(img,imgname,lim=40.,n=80,sat=0.7):
     print np.shape(img)
     plt.imshow(img,vmax=sat*np.max(img),extent=[lim,-lim,-lim,lim])
     plt.xlabel(r'x ($\mu$as)'); plt.ylabel(r'y ($\mu$as)')
-    plt.savefig(imgname+'_model_image.pdf')
+    plt.savefig(imgname+'_model_image.pdf',bbox_inches='tight',pad_inches=0.)
 
 # return set of scores
 def calc_likelihood(ruv,visamp,visamperr,cphase,sigmacp,lpamp,lpamperr,obs,obs_sim_perfect,cut_ruv=0.5e9,N=10):
@@ -98,6 +101,7 @@ def calc_likelihood(ruv,visamp,visamperr,cphase,sigmacp,lpamp,lpamperr,obs,obs_s
     print cut_ruv, np.min(ruv), len(good), len(ruv)
     chi2_self_amp=np.sum((visamp-obs_sim_perfect.unpack('amp')['amp'])**2./visamperr**2.)/len(visamp)
     diff=cphase-obs_sim_perfect.cphase['cphase']
+# account for wrapping closure phases
     diff[diff > 180.]=360.-diff[diff > 180.]
     chi2_self_cphase=np.sum(diff**2./sigmacp**2.)/len(cphase)
     chi2_amp=np.sum((visamp[good]-obs.unpack('amp')['amp'][good])**2./visamperr[good]**2.)/len(good)
@@ -117,6 +121,7 @@ def get_values(obs_list,visamperr_min=0.):
     ruv=np.sqrt(obs_list[0].data['u']**2.+obs_list[0].data['v']**2.)
     visamp_list = []; cphase_list = []
     visamp = []; cphase = []
+    lpamp = []; lpamp_list = []
 #visamp=np.array(visamp); cphase=np.array(cphase)
     obs_list[0].add_cphase()
     uu=obs_list[0].data['u']; vv=obs_list[0].data['v']
@@ -132,13 +137,16 @@ def get_values(obs_list,visamperr_min=0.):
     for o in obs_list:
 #        o.add_cphase()
 #        o.avg_coherent(0.,scan_avg=True)
-        visamp_list.append(np.abs(o.data['vis']))
+        visamp_list.append(o.unpack('amp')['amp'])
+        lpamp_list.append(np.sqrt(o.unpack('qamp')['qamp']**2.+o.unpack('uamp')['uamp']**2.))
         cphase_list.append(o.cphase['cphase'])
     print np.shape(visamp_list), np.shape(np.std(visamp_list,axis=0)), np.shape(np.median(visamp_list,axis=0))
     visamp=np.median(visamp_list,axis=0); cphase=np.median(cphase_list,axis=0)
+    lpamp=np.median(lpamp_list,axis=0)
     visamperr=np.sqrt((np.std(visamp_list,axis=0)/np.sqrt(N-1.))**2.+visamperr_min**2.)
+    lpamperr=np.sqrt((np.std(visamp_list,axis=0)/np.sqrt(N-1.))**2.+visamperr_min**2.)
     sigmacp=np.std(cphase_list,axis=0)/np.sqrt(N-1.)
-    return visamp,cphase,visamperr,sigmacp,uvdist,uu,vv,uvdistmax
+    return visamp,cphase,lpamp,visamperr,sigmacp,lpamperr,uvdist,uu,vv,uvdistmax
 
 #def plot_model_data(ruv,model,vis,viserr,plot_name,names=['visamp','cphase','logcamp'],ylabels=['visibility amplitude','closure phase (deg)','log closure amplitude'],xlabels=[r'uv distance (G$\lambda$)',r'maximum uv distance (G$\lambda$)']):
 #    for i in range(len(ruv)):
@@ -148,15 +156,19 @@ def get_values(obs_list,visamperr_min=0.):
 #        plt.plot(ruv[i]/1e9,model[i],marker='o',linestyle='')
 #        plt.savefig(plot_name+'_'+names[i]+'_model_data.pdf')
 
-def plot_model_data(o,ruv,visamp,visamperr,uvdistmax,cphase,sigmacp,plotname):
+# i think it's too confusing here having two sets of error bars? 
+def plot_model_data(o,ruv,visamp,visamperr,uvdistmax,cphase,sigmacp,lpamp,lpamperr,plotname,chi2amp=-1.,chi2cp=-1.,chi2lp=-1.):
 #    o.plotall('uvdist','amp')
     plt.figure(figsize=(5,4))
     plt.errorbar(ruv/1e9,visamp,visamperr,marker='o',linestyle='',zorder=1,label='simulated model')
     print np.shape(ruv), np.shape(o.data['vis'])
-    plt.errorbar(o.unpack('uvdist')['uvdist']/1e9,o.unpack('amp')['amp'],o.unpack('sigma')['sigma'],marker='o',linestyle='',label='data',zorder=3)
+#    plt.errorbar(o.unpack('uvdist')['uvdist']/1e9,o.unpack('amp')['amp'],o.unpack('sigma')['sigma'],marker='o',linestyle='',label='data',zorder=3)
+    plt.plot(o.unpack('uvdist')['uvdist']/1e9,o.unpack('amp')['amp'],marker='o',linestyle='',label='data',zorder=3)
     plt.axis([-0.5,9,-0.05,0.8])
     plt.legend(loc='upper right')
     plt.xlabel(r'uv distance (G$\lambda$)'); plt.ylabel('visibility amplitude (Jy)')
+    if chi2amp > 0:
+        plt.text(6,0.55,r'$\chi^2 = {0:.1f}$'.format(chi2amp))
     plt.savefig(plotname+'_comparison_visamp.pdf',bbox_inches='tight',pad_inches=0.)
     plt.figure(figsize=(5,4))
     plt.errorbar(uvdistmax/1e9,cphase,sigmacp,marker='o',linestyle='',zorder=1,label='simulated model')
@@ -164,9 +176,22 @@ def plot_model_data(o,ruv,visamp,visamperr,uvdistmax,cphase,sigmacp,plotname):
     plt.plot(uvdistmax/1e9,o.cphase['cphase'],marker='o',linestyle='',label='data',zorder=3)
     plt.axis([-0.5,9,-190.,190.])
 #legends, handles = plt.
-    plt.legend(loc='upper right')
+    plt.legend(loc='upper left')
     plt.xlabel(r'max uv distance (G$\lambda$)'); plt.ylabel('closure phase (deg)')
+    if chi2cp > 0:
+        plt.text(0,85.,r'$\chi^2 = {0:.1f}$'.format(chi2cp))
     plt.savefig(plotname+'_comparison_cphase.pdf',bbox_inches='tight',pad_inches=0.)
+    plt.figure(figsize=(5,4))
+    plt.errorbar(ruv/1e9,visamp,visamperr,marker='o',linestyle='',zorder=1,label='simulated model')
+    print np.shape(ruv), np.shape(o.data['vis'])
+    plt.plot(o.unpack('uvdist')['uvdist']/1e9,np.sqrt(o.unpack('qamp')['qamp']**2.+o.unpack('uamp')['uamp']**2.),marker='o',linestyle='',label='data',zorder=3)
+    plt.axis([-0.5,9,-0.02,0.14])
+    plt.legend(loc='upper right')
+    plt.xlabel(r'uv distance (G$\lambda$)'); plt.ylabel('visibility amplitude (Jy)')
+    print 'chi2lp: ', chi2lp
+    if chi2lp > 0:
+        plt.text(6,0.09,r'$\chi^2 = {0:.1f}$'.format(chi2lp))
+    plt.savefig(plotname+'_comparison_lpamp.pdf',bbox_inches='tight',pad_inches=0.)
 #plt.figure(figsize=(5,4))
 #plt.errorbar(uvdistmax,np.median(cphase,axis=0),np.std(cphase,axis=0)/3.,marker='o',linestyle='',zorder=1,label='median simulated')
 #plt.plot(uvdistmax,obs_sim_scan_perfect.cphase,marker='o',linestyle='',zorder=2,label='truth model')
